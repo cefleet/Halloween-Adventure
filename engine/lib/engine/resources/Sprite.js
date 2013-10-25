60149244c61789944cdae991d9395b4251ef5c26
cefleet <clint.fleetwood@gmail.com>
2013-10-25 07:36:33 -0400

    made the engine a module

--- engine/lib/engine/resources/Sprite.js
+++
@@ -1,9 +0,0 @@
-Game.Resource.Image.Sprite = new Game.Class(Game.Resource.Image,{
-	
-	initialize: function(options){
-		
-		Game.Resource.Image.prototype.initialize.apply(this,[options]);
-		this.type = options.type || 'sprite';
-	}
-	
-});
